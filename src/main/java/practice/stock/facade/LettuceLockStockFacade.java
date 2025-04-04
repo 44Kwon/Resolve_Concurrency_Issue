af11534a369d292b04c432bff7382ed8f70be975
44Kwon <kwh3217@gmail.com>
2025-04-04 16:46:58 +0900

    Redis의 Lettuce를 이용한 동시성 문제 해결 및 테스트

---
+++ src/main/java/practice/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,30 @@
+package practice.stock.facade;
+
+import org.springframework.stereotype.Component;
+import practice.stock.repository.RedisLockRepository;
+import practice.stock.service.StockService;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        // 락 획득에 실패하면 sleep 후 재시도
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
